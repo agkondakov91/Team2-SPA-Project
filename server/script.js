f4151b0152487f9669808130c3bfe09a49a7525d
EvgPov <pev-elets@yandex.ru>
2025-06-11 14:25:35 +0300

    add real data to db.json

--- server/script.js
+++ server/script.js
@@ -5,9 +5,9 @@
 import { api } from './api.js';
 
 import { POST, DELETE, PUT, GET } from './constants.js';
-// import { SERVER_URL_PICTURES_SECOND } from './constants.js';
+import { SERVER_URL_DATA, SERVER_URL_BLOGS } from './constants.js';
 
-// import { genUniqueId } from './general.js';
+import { genUniqueId } from './general.js';
 
 /** Функция получения всех данных с сервера.
  * @memberof module:script
@@ -237,3 +237,101 @@ export const removeData = async (endpoint, id) => {
 // addData(SERVER_URL_PICTURES_SECOND, addElement);
 // updateData(SERVER_URL_PICTURES_SECOND, updateElement);
 // removeData(SERVER_URL_PICTURES_SECOND, '11135');
+
+const data = await getData(SERVER_URL_DATA);
+
+//const titleSection1 = data.titleSection1; // заголовок Секции 1
+const { titleSection1 } = data;
+console.log(titleSection1, "\n");
+
+for (let i = 0; i < data.dataSection1_2.length; i++) {
+  console.log(i + 1, data.dataSection1_2[i].tourName); // список "Локация для тура"
+}
+console.log("\n");
+// по кнопке "Найти программу"
+const direction = data.dataSection1_2[0].tourDirections // массив направлений по выбранной локации
+
+for (let i = 0; i < direction.length; i++) {
+  console.log(i + 1, direction[i].dirName); // направления по выбранной локации
+}
+console.log("\n");
+
+const titleSection2 = data.dataSection1_2[0].titleSection2; //заголовок Секции 2
+console.log('Заголовок секции2: ', titleSection2);
+
+const subtitleSection2 = data.dataSection1_2[0].subtitleSection2; //подзаголовок Секции 2
+console.log('Подзаголовок секции 2: ', subtitleSection2, "\n");
+
+const textSection2 = data.dataSection1_2[0].tourStructure; // структура туров выбранного направления
+console.log('Структура тура: ', textSection2, "\n");
+
+// по кнопке "Программа тура"
+const program = direction[0].dirProgram; //  прграмма ппервого направления  по выбранной локации
+for (let i = 0; i < program.length; i++) {
+  console.log(i + 1, program[i].name); //
+}
+console.log("\n");
+
+const titleSection3 = data.dataSection3.titleSection3 // заголовок Секции 3
+console.log('Заголовок секции 3: ',titleSection3);
+
+const subtitleSection3 = data.dataSection3.subtitleSection3 // подзаголовк секции 3
+console.log('Подзаголовк секции 3:', subtitleSection3, "\n");
+
+const baseServices = data.dataSection3.baseServices;
+for (let i = 0; i < baseServices.length; i++) {
+  console.log(i + 1, baseServices[i].name); 
+  console.log(baseServices[i].description); 
+}
+console.log("\n");
+
+const titleSection4 = data.dataSection4.titleSection4; // заголовок Секции 4
+console.log('Заголовок секции 4: ',titleSection4);
+
+const subtitleSection4 = data.dataSection4.subtitleSection4 // подзаголовк секции 4
+console.log('Подзаголовк секции 4:', subtitleSection4, "\n");
+
+// данные для Секции 4
+const dataSectrion4 = [];
+dataSectrion4[0] = {
+  "tourName": data.dataSection1_2[0].tourDirections[1].dirName,
+  "tourDescription": data.dataSection1_2[0].tourDirections[1].dirDescription,
+  "tourPrice": data.dataSection1_2[0].tourDirections[1].dirPrice,
+};
+dataSectrion4[1] = {
+  "tourName": data.dataSection1_2[1].tourDirections[0].dirName,
+  "tourDescription": data.dataSection1_2[1].tourDirections[0].dirDescription,
+  "tourPrice": data.dataSection1_2[1].tourDirections[0].dirPrice,
+};
+dataSectrion4[2] = {
+  "tourName": data.dataSection1_2[2].tourDirections[4].dirName,
+  "tourDescription": data.dataSection1_2[2].tourDirections[4].dirDescription,
+  "tourPrice": data.dataSection1_2[2].tourDirections[4].dirPrice,
+};
+console.log('Данные секции 4: ', dataSectrion4);
+
+// БЛОГ
+// получение данных для блога
+const blogs = await getData(SERVER_URL_BLOGS);
+
+const titleBlogs = blogs.titleBlogs; // заголовок для Секции 5 - Блог
+console.log('Подзаголовк секции 3:', titleBlogs, "\n");
+
+const subtitleBlogs = blogs.subtitleBlogs; // подзаголовок для Секции 5 - Блог
+console.log('Подзаголовк секции 3:', subtitleBlogs, "\n");
+
+const dataBlogs = blogs.dataBlogs; // данные блога о путешествиях
+for (let i = 0; i < dataBlogs.length; i++) {
+  console.log(i + 1, dataBlogs[i].blogName); 
+  console.log(dataBlogs[i].blogText); 
+}
+console.log("\n");
+
+//console.log(data);
+//getDataById (SERVER_URL_PICTURES_SECOND, '101');
+// const add = await addData(SERVER_URL_TODOS, addElement);
+// console.log(add);
+// const data = await getData(SERVER_URL_TODOS);
+// console.log(data);
+// updateData(SERVER_URL_PICTURES_SECOND, updateData);
+ //removeData(SERVER_URL_PICTURES_SECOND, '11135');
